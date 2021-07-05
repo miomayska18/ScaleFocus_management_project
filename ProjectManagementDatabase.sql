2f7bdf1bdc34ce1f828b7482d9afe48250911ed9
miomayska18 <miomayska18@codingburgas.bg>
2021-07-05 14:51:54 +0300

    Create database and User table

---
+++ ProjectManagementDatabase.sql
@@ -0,0 +1,100 @@
+USE [ProjectManagement]
+GO
+/****** Object:  Database [ProjectManagement]    Script Date: 7/5/2021 2:45:00 PM ******/
+CREATE DATABASE [ProjectManagement]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'ProjectManagement', FILENAME = N'C:\Users\miomayska18\ProjectManagement.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'ProjectManagement_log', FILENAME = N'C:\Users\miomayska18\ProjectManagement_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [ProjectManagement].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [ProjectManagement] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [ProjectManagement] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [ProjectManagement] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [ProjectManagement] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [ProjectManagement] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [ProjectManagement] SET  MULTI_USER 
+GO
+ALTER DATABASE [ProjectManagement] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [ProjectManagement] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [ProjectManagement] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [ProjectManagement] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [ProjectManagement] SET DELAYED_DURABILITY = DISABLED 
+GO
+USE [ProjectManagement]
+GO
+/****** Object:  Table [dbo].[User]    Script Date: 7/5/2021 2:45:00 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[User](
+	[Id] [int] NOT NULL,
+	[UserName] [nvarchar](50) NOT NULL,
+	[Password] [varbinary](50) NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[DateOfCreation] [datetime] NOT NULL,
+	[IdOfCreator] [int] NOT NULL,
+	[DateLastChange] [datetime] NOT NULL,
+	[IdLastChange] [int] NOT NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+USE [master]
+GO
+ALTER DATABASE [ProjectManagement] SET  READ_WRITE 
+GO
